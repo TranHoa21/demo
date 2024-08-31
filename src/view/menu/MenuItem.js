2a488b9951528cb57f3012903ee5e20a288bd976
TranHoa21 <soc2k3dangyeu@gmail.com>
2024-08-31 23:29:07 +0700

    up2

--- src/view/menu/MenuItem.js
+++ src/view/menu/MenuItem.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../../style/menu/navmenu.scss"
+import "../../style/menu/menuitem.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 
@@ -22,15 +22,32 @@ const MenuItem = () => {
     }, []);
 
     if (loading) return <div>Loading...</div>;
+    const chunkedData = [];
+    for (let i = 0; i < menuData.length; i += 4) {
+        chunkedData.push(menuData.slice(i, i + 4));
+    }
     return (
         <>
-            <div className="sp-menu-container">
-                {menuData.map(item => (
-                    <div key={item.id}>
-                        <h3>{item.name}</h3>
-                        <p>{item.describe}</p>
-                        <p>Price: ${item.price}</p>
-                        <img src={item.img} alt={item.name} />
+            <div className="sp-container">
+                {chunkedData.map((chunk, index) => (
+                    <div key={index} className="sp-menu row">
+                        {chunk.map(item => (
+                            <div key={item.id} className="sp-menu-item col-sm-3">
+                                <img className="sp-image" src={item.img} alt={item.name} />
+                                <h5 className="sp-menu-item-title">{item.name}</h5>
+                                <div className="list-product-item-star">
+                                    <p className="list-product-item-price">Price: ${item.price}</p>
+                                    <div className="list-product-item-review">
+                                        <img className="list-product-item-review-icon" src="https://res.cloudinary.com/dhjrrk4pg/image/upload/v1716638512/star_l6gizo.png" />
+                                        <img className="list-product-item-review-icon" src="https://res.cloudinary.com/dhjrrk4pg/image/upload/v1716638512/star_l6gizo.png" />
+                                        <img className="list-product-item-review-icon" src="https://res.cloudinary.com/dhjrrk4pg/image/upload/v1716638512/star_l6gizo.png" />
+                                        <img className="list-product-item-review-icon" src="https://res.cloudinary.com/dhjrrk4pg/image/upload/v1716638512/star_l6gizo.png" />
+                                        <img className="list-product-item-review-icon" src="https://res.cloudinary.com/dhjrrk4pg/image/upload/v1716638512/star_l6gizo.png" />
+                                    </div>
+
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 ))}
             </div>
